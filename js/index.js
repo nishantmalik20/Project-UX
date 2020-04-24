1bfbebfcabedc7e4e65fd59ab74da5a92afaef87
Nishant Malik <60120827+nishantmalik20@users.noreply.github.com>
2020-04-24 16:13:53 -0400

    Added Scrollbar with JS

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    console.log('in fn');
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  console.log(scrolled);
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("header-nav").style.zIndex="1";
-}, false);
+       
+
+ }, false);
 
  
