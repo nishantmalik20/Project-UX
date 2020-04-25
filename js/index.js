e7a799eea27ef162aa97af4e56e37713790dd2e2
Nishant Malik <60120827+nishantmalik20@users.noreply.github.com>
2020-04-25 13:56:43 -0400

    Headers fixed relative to articles

--- js/index.js
+++ js/index.js
@@ -1,18 +1,36 @@
-//Code by jobanjit with the help of www.w3schools.com
+//Code by jobanjit
+var header = document.getElementById("myHeader-leg");
+var header2 = document.getElementById("myHeader-per");
+var sticky = header.offsetTop;
+var sticky2 = header2.offsetTop;
+
 window.onscroll = function() {myFunction()};
+
 function myFunction() {
-    console.log('in fn');
+  console.clear();
+  console.log(window.pageYOffset);
+  console.log(sticky);
+  if (window.pageYOffset+75 > sticky && window.pageYOffset <3650 ) {
+    header2.classList.remove("sticky");
+    header.classList.add("sticky");
+  }
+  else if(window.pageYOffset > 3651 )
+  {
+    header.classList.remove("sticky");
+    header2.classList.add("sticky");  
+  } 
+  else {
+    header.classList.remove("sticky");
+  }
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
-  console.log(scrolled);
+
   document.getElementById("myBar").style.width = scrolled + "%";
 }
 
 //Code by Jobanjit Singh and Nishant Malik
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("header-nav").style.zIndex="1";     
+    document.getElementById("header-nav").style.zIndex="1";   
 
  }, false);
-
- 
